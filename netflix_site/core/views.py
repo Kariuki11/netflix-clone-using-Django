4a663afcf6da4c478f8aacb6e3dbccef6139a405
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 05:38:22 +0300

    New changes

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -2,10 +2,16 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    movies = Movie.objects.all()
+    
+    context = {
+        'movies': movies,
+    }
+    return render(request, 'index.html', context)
 
 def login(request):
     if request.method == 'POST':
