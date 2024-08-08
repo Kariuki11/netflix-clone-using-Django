7a3ff283cf78ddbd35df7cdc1872ba7d7f22d8f2
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 04:16:49 +0300

    New changes

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -16,6 +16,19 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
         password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
     else:
         return render(request, 'signup.html')
 
