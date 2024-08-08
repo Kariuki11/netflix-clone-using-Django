b136cdaa3ecfc411d65b7e66ecde56f8f964376e
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 03:07:30 +0300

    New changes

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -5,8 +5,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def add_to_list(request):
-    if request.method == 'POST':
-        # Your logic to add the item to the list
-        return JsonResponse({'message': 'Item added to list successfully'})
-    return JsonResponse({'message': 'Invalid request'}, status=400)
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+# def add_to_list(request):
+#     if request.method == 'POST':
+#         # Your logic to add the item to the list
+#         return JsonResponse({'message': 'Item added to list successfully'})
+#     return JsonResponse({'message': 'Invalid request'}, status=400)
