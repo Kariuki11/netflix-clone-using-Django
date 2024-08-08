8e1c40950b13fcab9c1dc6be5562178e678ac1ed
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 04:08:45 +0300

    New changes

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,7 +11,13 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+    else:
+        return render(request, 'signup.html')
 
 def add_to_list(request):
     if request.method == 'POST':
