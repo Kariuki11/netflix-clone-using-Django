39339efa5eed54ca40e6cc46612996353fc735e3
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 05:10:16 +0300

    New changes

--- netflix_site/core/models.py
+++ netflix_site/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Movie(models.Model):
+#class Movie(models.Model)
+    
+    GENRE_CHOICES = [
+        ('Action', 'Action'),
+        ('Comedy', 'Comedy'),
+        ('Drama', 'Drama'),
+        ('Horror', 'Horror'),
+        ('Romance', 'Romance'),
+        ('science_fiction', 'Science Fiction'),
+        ('fantasy', 'Fantasy'),
+    ]
+    uu_id = models.UUIDField(default=uuid.uuid4)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
+    length = models.PositiveIntegerField()
+    image_card = models.ImageField(upload_to='movie_images/')
+    image_cover = models.ImageField(upload_to='movie_images/')
+    video = models.FileField(upload_to='movie_videos/')
+    movie_views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
--- netflix_site/netflix_site/settings.py
+++ netflix_site/netflix_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
