06baea8b473bb4b897690b98ca8521a4bd8a6f01
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-09 00:36:49 +0300

    New changes

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -30,8 +30,14 @@ def movie(request, pk):
 
 @login_required(login_url='login')
 def my_list(request):
+    
+    movie_list = MovieList.objects.filter(owner_user=request.user)
+    user_movie_list = []
+    
+    for movie in movie_list:
+        user_movie_list.append(movie.movie)
     context = {
-        
+        'movies': user_movie_list
     }
     return render(request, 'my_list.html', context)
 
