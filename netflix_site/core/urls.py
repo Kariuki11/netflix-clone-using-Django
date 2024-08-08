ea8c81ae94dc0423839b37b45a3dbf4c7a6fd656
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 14:06:29 +0300

    New commits

--- netflix_site/core/urls.py
+++ netflix_site/core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('movie/<str:pk>/', views.movie, name='movie'),
+    path('my-list', views.my_list, name='my-list'),
+    path('add-to-list', views.add_to_list, name='add-to-list'),
 ]
