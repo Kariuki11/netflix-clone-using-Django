953d0f2d0e1c01378230b8e5e537c84aa47c861a
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-09 00:24:42 +0300

    New commits

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -28,9 +28,14 @@ def movie(request, pk):
     
     return render(request, 'movie.html', context)
 
+@login_required(login_url='login')
 def my_list(request):
+    context = {
+        
+    }
     return render(request, 'my_list.html', context)
 
+@login_required(login_url='login')
 def add_to_list(request):
     if request.method == 'POST':
         movie_url_id = request.POST.get('movie_id')
