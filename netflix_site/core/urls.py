bea0cf992c7fd41521831623b0a71ad39b01174f
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-08 03:36:10 +0300

    New change

--- netflix_site/core/urls.py
+++ netflix_site/core/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add-to-list/', views.add_to_list, name='add-to-list'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
